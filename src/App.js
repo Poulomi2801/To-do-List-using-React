18d23414cd7785096bc284d4d52a61e78a6432a9
Poulomi Das <poulomidas2801@gmail.com>
2025-04-14 02:02:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Resturant from './component/basics/Resturant'
+// import UseState from './component/Hooks/useState';
+// import UseEffect from "./component/Hooks/useEffect"
+// import UseReducer from './component/Hooks/useReducer';
+import Todo from './component/todoreact/todo';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+const App = () => {
+  return (
+    <>
+      <Todo/>
+    </>
+  );
+};
+
+export default App
